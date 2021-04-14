379bb0aea6aa4e110b493654ea3a13deeecfd051
Marsel Estefan Lie <marselestefan98@gmail.com>
2021-04-14 14:18:38 +0700

    - add third page

---
+++ AIA iOS App Showcase/Controller/ThirdPage.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdPage.swift
+//  AIA iOS App Showcase
+//
+//  Created by Marsel Estefan Lie on 14/04/21.
+//
+
+import UIKit
+
+class ThirdPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
